3c03dcbb9b9d449a2f57e37f38136c00dbaa6bde
Payton Dennis <dennispayton15@gmail.com>
2023-08-12 09:56:17 -0700

    [refactor] Format code with Prettier ext

--- frontend/screens/HomeScreen.tsx
+++ frontend/screens/HomeScreen.tsx
@@ -1,66 +1,65 @@
-import React from 'react';
-import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, SafeAreaView } from "react-native";
 
 export type Props = {};
 
-const HomeScreen : React.FC<Props> = () => { 
-    return (
-        <SafeAreaView>
-            {/* Welcome Header */}
-            <SafeAreaView style={styleWelcomeHeader.container}>
-                <Text>Welcome Back</Text>
-                <Text style={styleWelcomeHeader.name}>Payton</Text>
-            </SafeAreaView>
+const HomeScreen: React.FC<Props> = () => {
+  return (
+    <SafeAreaView>
+      {/* Welcome Header */}
+      <SafeAreaView style={styleWelcomeHeader.container}>
+        <Text>Welcome Back</Text>
+        <Text style={styleWelcomeHeader.name}>Payton</Text>
+      </SafeAreaView>
 
-            {/* Badges */}
-            <SafeAreaView style={styleBadges.container}>
-                <Text style={styleBadges.header}>Badges</Text>
-            </SafeAreaView>
+      {/* Badges */}
+      <SafeAreaView style={styleBadges.container}>
+        <Text style={styleBadges.header}>Badges</Text>
+      </SafeAreaView>
 
-            {/* Pinned */}
-            <SafeAreaView >
-                <Text>Pinned</Text>
-            </SafeAreaView>
+      {/* Pinned */}
+      <SafeAreaView>
+        <Text>Pinned</Text>
+      </SafeAreaView>
 
-            {/* Prayer card */}
-            <SafeAreaView >
-                <Text>Scrolling inside prayer card (not a scrollview)</Text>
-            </SafeAreaView>
+      {/* Prayer card */}
+      <SafeAreaView>
+        <Text>Scrolling inside prayer card (not a scrollview)</Text>
+      </SafeAreaView>
 
-            {/* Notes */}
-            <SafeAreaView >
-                <Text>Notes</Text>
-            </SafeAreaView>
+      {/* Notes */}
+      <SafeAreaView>
+        <Text>Notes</Text>
+      </SafeAreaView>
 
-            {/* Highlights */}
-            <SafeAreaView >
-                <Text>Highlights</Text>
-            </SafeAreaView>
-
-        </SafeAreaView>
-    );
+      {/* Highlights */}
+      <SafeAreaView>
+        <Text>Highlights</Text>
+      </SafeAreaView>
+    </SafeAreaView>
+  );
 };
 
 const styleWelcomeHeader = StyleSheet.create({
-    container: {
-        marginLeft: 20,
-    },
+  container: {
+    marginLeft: 20,
+  },
 
-    name: {
-      fontSize: 40,
-      fontWeight: 'bold',
-    },
-  });
+  name: {
+    fontSize: 40,
+    fontWeight: "bold",
+  },
+});
 
 const styleBadges = StyleSheet.create({
-    container: {
-        margin: 15,
-    },
+  container: {
+    margin: 15,
+  },
 
-    header: {
-        fontSize: 30,
-        fontWeight: 'bold',
-    }
+  header: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
 });
 
 export default HomeScreen;
