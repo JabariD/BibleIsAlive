fda5a9243a46f4e4f3d476f5c1aed215211cb1ec
Payton Dennis <dennispayton15@gmail.com>
2023-09-09 10:34:27 -0700

    [feat] Add theme for colors and styling
    #css

---
+++ frontend/utils/theme.ts
@@ -0,0 +1,21 @@
+// Color palette used throughout the app.
+const palette = {
+  gold: "#EDD47C",
+  blue: "#152D3C",
+  white: "#FFFFFF",
+  red: "#F03030",
+};
+
+// Consistent theme for the app.
+export const theme = {
+  colors: {
+    primary: palette.blue,
+    secondary: palette.gold,
+    danger: palette.red,
+  },
+  textVariants: {
+    primary: {
+      fontFamily: "Gilroy",
+    },
+  },
+};
