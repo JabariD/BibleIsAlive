ceb1342216203e136ad80a7751f5a7894518be8e
Payton Dennis <dennispayton15@gmail.com>
2023-09-09 11:25:59 -0700

    [feat] Add custom header bar to allow for icons and coloring
    #css #ui

--- frontend/App.js
+++ frontend/App.js
@@ -1,7 +1,7 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, Image } from "react-native";
+import { StyleSheet, Text, Image, SafeAreaView, StatusBar } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { theme } from "./utils/theme";
 
 import HomeScreen from "./screens/HomeScreen";
 
@@ -10,9 +10,41 @@ const Tab = createBottomTabNavigator();
 // Temporary to assume user signed in.
 const isSignedIn = true;
 
+// See https://reactnavigation.org/docs/bottom-tab-navigator#header-related-options for props.
+function HeaderBar(props) {
+  return (
+    <SafeAreaView style={stylesHeader.container}>
+      <SafeAreaView>
+      <Image
+          source={require("./assets/icons/bookmark.png")}
+          style={{
+            width: 30,
+            height: 30,
+            marginLeft: 10,
+          }}
+        />
+      </SafeAreaView>
+      <Text style={stylesHeader.text}>{props.options.tabBarLabel}</Text>
+      <SafeAreaView>
+        <Image
+          source={require("./assets/icons/user.png")}
+          style={{
+            width: 30,
+            height: 30,
+            marginRight: 10,
+          }}
+        />
+      </SafeAreaView>
+
+    </SafeAreaView>
+  );
+}
+
 export default function App() {
   return (
     <NavigationContainer>
+      {/* Make StatusBar white. */}
+      <StatusBar barStyle="light-content" />
       {/* <HomeScreen /> */}
       {isSignedIn ? (
         <Tab.Navigator>
@@ -20,7 +52,11 @@ export default function App() {
             name="Home"
             component={HomeScreen}
             options={{
+              // Header bar
+              header: (props) => <HeaderBar {...props} />,
+              // Label of tab icon
               tabBarLabel: "Home",
+              // Image of icon
               tabBarIcon: ({ focused, color, size }) => (
                 <Image
                   source={require("./assets/icons/home.png")}
@@ -50,5 +86,28 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+
+  customHeaderContainer: {
+    backgroundColor: "#000",
+  },
+});
+
+const stylesHeader = StyleSheet.create({
+  container: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+
+    backgroundColor: theme.colors.primary,
+    height: 100,
+  },
+
+  text: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 20,
   },
 });
