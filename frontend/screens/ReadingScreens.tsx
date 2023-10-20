58745887a5ac2cb4cac3f6423221fac584ae080d
Payton Dennis <dennispayton15@gmail.com>
2023-10-20 07:42:37 -0700

    [feat] Add User Post to Bible Reading Screen
    #css #ui

--- frontend/screens/ReadingScreens.tsx
+++ frontend/screens/ReadingScreens.tsx
@@ -9,6 +9,35 @@ import {
   TouchableOpacity,
 } from "react-native";
 
+const text =
+  "Then what is the advantage of the Jew? Or what is the benefit of circumcision? Great in every respect. To begin with, the Jews were entrusted with the oracles of God [His very words]. What then? If some did not believe or were unfaithful [to God], their lack of belief will not nullify and make invalid the faithfulness of God and His word, will it? Certainly not! Let God be found true [as He will be], though every person be found a liar, just as it is written [in Scripture],";
+
+interface UserPostProps {}
+const UserPost: React.FC<UserPostProps> = () => {
+  return (
+    <SafeAreaView style={trendingPostsStyles.container}>
+      {/* Image */}
+      <SafeAreaView>
+        <Image
+          source={require("../assets/images/profile-image.png")}
+          style={{ height: 40, width: 40, borderRadius: 30 }}
+        ></Image>
+      </SafeAreaView>
+      {/* Icon Type + Views */}
+      <SafeAreaView>
+        <Text>💙 212k</Text>
+      </SafeAreaView>
+    </SafeAreaView>
+  );
+};
+
+const trendingPostsStyles = StyleSheet.create({
+  container: {
+    display: "flex",
+    alignItems: "center",
+  },
+});
+
 export type Props = {};
 const ReadingScreen: React.FC<Props> = () => {
   return (
@@ -20,7 +49,19 @@ const ReadingScreen: React.FC<Props> = () => {
       </SafeAreaView>
 
       {/* Bible Text and recent posts */}
-      <SafeAreaView></SafeAreaView>
+      <SafeAreaView style={stylesBibleTextAndRecentPosts.container}>
+        {/* Text */}
+        <SafeAreaView style={stylesBibleTextAndRecentPosts.bibleTextContainer}>
+          <Text style={stylesBibleTextAndRecentPosts.bibleText}>{text}</Text>
+        </SafeAreaView>
+        {/* Recent Posts */}
+        <SafeAreaView style={stylesBibleTextAndRecentPosts.recentPostsContainer}>
+          <UserPost />
+          <UserPost />
+          <UserPost />
+        </SafeAreaView>
+      </SafeAreaView>
+
       {/* GPT icons */}
       <SafeAreaView></SafeAreaView>
     </SafeAreaView>
@@ -48,7 +89,28 @@ const stylesHeader = StyleSheet.create({
   },
 });
 
-const stylesBibleTextAndRecentPosts = StyleSheet.create({});
+const stylesBibleTextAndRecentPosts = StyleSheet.create({
+  container: {
+    marginTop: 30,
+    display: "flex",
+    flexDirection: "row",
+  },
+
+  bibleTextContainer: {
+    width: "85%",
+  },
+
+  bibleText: {
+    fontSize: 20,
+    lineHeight: 30,
+  },
+
+  recentPostsContainer: {
+    display: "flex",
+    flexDirection: "column",
+    gap: 70,
+  },
+});
 
 const stylesGPTIcons = StyleSheet.create({});
 
