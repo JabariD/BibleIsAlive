e4594d8c50cd81835a3f78556d0305a859728a54
Payton Dennis <dennispayton15@gmail.com>
2023-10-22 09:35:43 -0700

    [feat] Refactor and fix styling of Navigation
    - Allows us to refactor navigation to be more clean and fix styling.
    - Fix header bar styling in stack navigation apps
    - Add comments.
    
    #css #frontend #refactor

--- frontend/App.js
+++ frontend/App.js
@@ -1,10 +1,26 @@
-import { StyleSheet, Text, Image, SafeAreaView, StatusBar } from "react-native";
+/**
+ * This is the main entry point for the application.
+ * It sets up the navigation structure of the app using React Navigation.
+ * 
+ * The app uses a combination of stack navigation and tab navigation.
+ * The stack navigator is the root navigator, and one of its screens is the tab navigator.
+ * 
+ * The tab navigator has four screens: Home, Bible, Timeline, and Chat.
+ * Each tab screen is associated with an icon and a component that gets rendered when the tab is active.
+ * 
+ * The stack navigator has five screens: TabNavigationHome, Profile, Settings, Knowledge, and Reading.
+ * The TabNavigationHome screen is the tab navigator described above.
+ * The other screens are individual components that get rendered when navigating from the tab screens.
+ * 
+ * The output of this file is the main App component that renders the NavigationContainer wrapping the stack navigator.
+ */
+
+import { Image, StatusBar, Text } from "react-native";
+
+// React Native Navigation
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { theme } from "./utils/theme";
-
 import { createStackNavigator } from "@react-navigation/stack";
-import { TransitionSpecs } from "@react-navigation/stack";
 
 // Screens
 import HomeScreen from "./screens/HomeScreen";
@@ -19,100 +35,61 @@ import ReadingScreen from "./screens/ReadingScreens";
 // Components
 import HeaderBarComponent from "./components/HeaderBarComponent";
 
+// Icons
+import homeIcon from './assets/icons/home.png';
+import bibleIcon from './assets/icons/bible.png';
+import timelineIcon from './assets/icons/magnifying-glass.png';
+import chatIcon from './assets/icons/bubble-chat.png';
+
 const Tab = createBottomTabNavigator();
 
 /**
  * Renders a tab navigation component.
  *
+ * This component is used in the main App component to provide tab navigation functionality.
+ * It is used to navigate between the Home, Bible, Timeline, and Chat screens.
+ *
  * @return {ReactNode} The rendered tab navigation component.
  */
 const TabNavigation = () => {
+  const tabScreens = [
+    { name: "Home", component: HomeScreen, icon: homeIcon },
+    { name: "Bible", component: BibleScreen, icon: bibleIcon },
+    { name: "Timeline", component: TimelineScreen, icon: timelineIcon },
+    { name: "Chat", component: ChatScreen, icon: chatIcon },
+  ];
+
   return (
     <Tab.Navigator>
-      {/* HomeScreen */}
-      <Tab.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{
-          // Header bar
-          header: (props) => <HeaderBarComponent {...props} />,
-          // Label of tab icon
-          tabBarLabel: "Home",
-          // Image of icon
-          tabBarIcon: ({ focused, color, size }) => (
-            <Image
-              source={require("./assets/icons/home.png")}
-              style={{
-                width: 30,
-                height: 30,
-              }}
-            />
-          ),
-        }}
-      />
-      {/* BibleScreen */}
-      <Tab.Screen
-        name="Bible"
-        component={BibleScreen}
-        options={{
-          // Header bar
-          header: (props) => <HeaderBarComponent {...props} />,
-          // Label of tab icon
-          tabBarLabel: "Bible",
-          // Image of icon
-          tabBarIcon: ({ focused, color, size }) => (
-            <Image
-              source={require("./assets/icons/bible.png")}
-              style={{
-                width: 30,
-                height: 30,
-              }}
-            />
-          ),
-        }}
-      />
-      {/* TimelineScreen */}
-      <Tab.Screen
-        name="Timeline"
-        component={TimelineScreen}
-        options={{
-          // Header bar
-          header: (props) => <HeaderBarComponent {...props} />,
-          // Label of tab icon
-          tabBarLabel: "Explore",
-          // Image of icon
-          tabBarIcon: ({ focused, color, size }) => (
-            <Image
-              source={require("./assets/icons/magnifying-glass.png")}
-              style={{
-                width: 30,
-                height: 30,
-              }}
-            />
-          ),
-        }}
-      />
-      {/* ChatScreen */}
-      <Tab.Screen
-        name="Chat"
-        component={ChatScreen}
-        options={{
-          // Header bar
-          header: (props) => <HeaderBarComponent {...props} />,
-          // Label of tab icon
-          tabBarLabel: "Chat",
-          // Image of icon
-          tabBarIcon: ({ focused, color, size }) => (
-            <Image
-              source={require("./assets/icons/bubble-chat.png")}
-              style={{
-                width: 30,
-                height: 30,
-              }}
-            />
-          ),
-        }}
-      />
+      {tabScreens.map((screen) => (
+        <Tab.Screen
+          key={screen.name}
+          name={screen.name}
+          component={screen.component}
+          options={{
+            header: (props) => <HeaderBarComponent {...props} />,
+            tabBarLabel: ({ focused, color, size }) => (<Text style={{ tintColor: "gray", color: "gray" }}>{screen.name}</Text>),
+            tabBarIcon: ({ focused, color, size }) => (
+              <Image
+                source={screen.icon}
+                style={{
+                  width: size,
+                  height: size,
+                  tintColor: focused ? '#EDD47C' : 'gray', // Change color to blue when focused
+                }}
+                resizeMode="contain" // Ensure the icon images are resized properly
+              />
+            ),
+            tabBarLabelStyle: {
+              fontSize: 12, // Adjust the font size
+              marginBottom: 3, // Add some margin to the bottom for better alignment
+            },
+            tabBarIconStyle: {
+              marginTop: 3, // Add some margin to the top for better alignment
+            },
+          }}
+        />
+      ))}
     </Tab.Navigator>
   );
 };
@@ -120,9 +97,12 @@ const TabNavigation = () => {
 /**
  * Renders the main application component.
  *
+ * This component is used as the root component of the application. It sets up the navigation stack
+ * and includes the TabNavigation component for navigating between the Home, Bible, Timeline, and Chat screens.
+ * It also includes the StatusBar and other screens like Profile, Settings, Knowledge, and Reading.
+ *
  * @return {JSX.Element} The rendered application component.
  */
-
 const Stack = createStackNavigator();
 export default function App() {
   return (
@@ -130,40 +110,28 @@ export default function App() {
       {/* Make StatusBar white. or set to auto? */}
       <StatusBar barStyle="light-content" />
 
-      <Stack.Navigator>
-        <Stack.Screen
-          name="TabNavigationHome"
-          component={TabNavigation}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Profile"
-          component={ProfileScreen}
-          options={{
-            headerBackTitle: "Back",
-          }}
-        />
-        <Stack.Screen
-          name="Settings"
-          component={SettingsScreen}
-          options={{
-            headerBackTitle: "Back",
-          }}
-        />
-        <Stack.Screen
-          name="Knowledge"
-          component={KnowledgeScreen}
-          options={{
-            headerBackTitle: "Back",
-          }}
-        />
-        <Stack.Screen
-          name="Reading"
-          component={ReadingScreen}
-          options={{
-            headerBackTitle: "Back",
-          }}
-        />
+      {/* These options apply  to all stack navigator screens unless overriden.*/}
+      <Stack.Navigator screenOptions={{
+        headerStyle: {
+          backgroundColor: '#152D3C', // Set the header background color.
+        },
+        headerBackTitle: "Back", // Set the back title/
+        headerTintColor: 'white' // Set the tint color of the header.
+      }}>
+        {[
+          { name: "TabNavigationHome", component: TabNavigation, options: { headerShown: false } },
+          { name: "Profile", component: ProfileScreen, options: { headerBackTitle: "Back" } },
+          { name: "Settings", component: SettingsScreen, options: { headerBackTitle: "Back" } },
+          { name: "Knowledge", component: KnowledgeScreen, options: { headerBackTitle: "Back" } },
+          { name: "Reading", component: ReadingScreen, options: { headerBackTitle: "Back" } },
+        ].map((screen) => (
+          <Stack.Screen
+            key={screen.name}
+            name={screen.name}
+            component={screen.component}
+            options={screen.options}
+          />
+        ))}
       </Stack.Navigator>
     </NavigationContainer>
   );
